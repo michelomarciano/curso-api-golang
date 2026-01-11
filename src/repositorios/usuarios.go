a7e5055e3b3db3c58ca592116aeac123524357c1
michel oliveira marciano <micheloliveiramarciano@MacBook-Pro-de-michel.local>
2026-01-10 21:30:34 -0300

    feat: adiciona função Buscar para pesquisar usuários por nome ou nick

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -1,29 +1,30 @@
 package repositorios
 
 import (
-	"database/sql"
 	"api/src/modelos"
+	"database/sql"
+	"fmt"
 )
 
-//Usuarios representa um repositório de usuários
+// Usuarios representa um repositório de usuários
 type Usuarios struct {
 	db *sql.DB
 }
 
-//NovoRepositorioDeUsuarios cria um novo repositório de usuários
+// NovoRepositorioDeUsuarios cria um novo repositório de usuários
 func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
 	return &Usuarios{db}
 }
 
-//Criar cria um novo usuário
+// Criar cria um novo usuário
 func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 	statement, erro := repositorio.db.Prepare("INSERT INTO usuarios (nome, nick, email, senha) VALUES (?, ?, ?, ?)")
 	if erro != nil {
 		return 0, erro
 	}
 	defer statement.Close()
-	
-	resultado , erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
 	if erro != nil {
 		return 0, erro
 	}
@@ -34,4 +35,38 @@ func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 	}
 
 	return uint64(ultimoIdInserido), nil
+}
+
+func (repositorio Usuarios) Buscar(nomeOUNick string) ([]modelos.Usuario, error) {
+	nomeOUNick = fmt.Sprintf("%%%s%%", nomeOUNick)
+
+	linhas, erro := repositorio.db.Query(
+		"select id, nome, nick, email, criadoEm from usuarios where nome LIKE ? or nick LIKE ?",
+		nomeOUNick,
+		nomeOUNick,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var usuario modelos.Usuario
+
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
 }
